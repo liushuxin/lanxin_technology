32aab45851bd99062781d61b02afe5c017bb7e68
liushuxin <shuxinliu2016@gmail.com>
2016-11-14 10:42:46 +0800

    add language configuration

--- public/javascripts/datatables/index.js
+++ public/javascripts/datatables/index.js
@@ -107,9 +107,35 @@
 			value: function paintTable() {
 				var self = this;
 				self.state.$datatable = $(".main-tb").DataTable({
-					data: self.state.data,
-					retrieve: true,
-					"columns": [{ "data": "_id", "title": "数据id" }, { "data": "name", "title": "姓名" }, { "data": "age", "title": "年龄" }]
+					"data": self.state.data, //渲染数据
+					"retrieve": true, //是否返回引用
+					"columns": [//列定义
+					{ "data": "_id", "title": "数据id" }, { "data": "name", "title": "姓名" }, { "data": "age", "title": "年龄" }],
+					"lengthMenu": [5, 10, 15, 20, 100],
+					"pagingType": "full_numbers",
+					"language": {
+						"emptyTable": "没有查询到相应数据@_@",
+						"info": "从第 _START_ 条到第 _END_条，共计 _TOTAL_ 条数据",
+						"infoEmpty": "共0条数据",
+						"infoFiltered": "(filtered from _MAX_ total entries)",
+						"infoPostFix": "",
+						"thousands": ",",
+						"lengthMenu": "每页显示 _MENU_ 条数据",
+						"loadingRecords": "加载中...",
+						"processing": "正在进行中...",
+						"search": "搜索:",
+						"zeroRecords": "没有搜索到你想要的记录",
+						"paginate": {
+							"first": "第一页",
+							"last": "最后一页",
+							"next": "下一页",
+							"previous": "上一页"
+						},
+						"aria": {
+							"sortAscending": ": activate to sort column ascending",
+							"sortDescending": ": activate to sort column descending"
+						}
+					}
 				});
 				self.state.$datatable.draw();
 			}
