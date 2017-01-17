9a350581430b19bb5e0dd0fd5cf0ec4737c58166
liushuxin <shuxinliu2016@gmail.com>
2017-01-17 21:36:34 +0800

    add dragula

--- dist/js/user_info/index.js
+++ dist/js/user_info/index.js
@@ -1,14 +1,46 @@
 import React,{Component,propTypes} from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import {Provider} from 'react-redux';
-import {reducer} from '../../components/reducer';
 import $ from 'jquery';
-import App from '../../components/App'; 
-import IndexSass from '../../sass/index.scss'; 
-const store=createStore(reducer); 
- console.log(store.getState()); 
- ReactDOM.render(<Provider store={store}>  
- <App/>
-  </Provider>,
- document.getElementById('app') );
+import 'dragula/dragula';
+import dragula from 'dragula';
+import IndexSass from '../../sass/index.scss';
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state ={};
+  }
+  componentDidMount(){
+    dragula(document.getElementById('dr'), {
+  isContainer: function (el) {
+    return false; // only elements in drake.containers will be taken into account
+  },
+  moves: function (el, source, handle, sibling) {
+    return true; // elements are always draggable by default
+  },
+  accepts: function (el, target, source, sibling) {
+    return true; // elements can be dropped in any of the `containers` by default
+  },
+  invalid: function (el, handle) {
+    return false; // don't prevent any drags from initiating by default
+  },
+  direction: 'vertical',             // Y axis is considered when determining where an element would be dropped
+  copy: false,                       // elements are moved by default, not copied
+  copySortSource: false,             // elements in copy-source containers can be reordered
+  revertOnSpill: false,              // spilling will put the element back where it was dragged from, if this is true
+  removeOnSpill: false,              // spilling will `.remove` the element, if this is true
+  mirrorContainer: document.body,    // set the element that gets mirror elements appended
+  ignoreInputTextSelection: true     // allows users to select input text, see details below
+});
+  }
+  render(){
+    let self = this;
+    return (<div id="dr">
+    
+    拖拽
+    div
+    </div>);
+  }
+
+} 
+ ReactDOM.render(<App/>,
+ document.getElementById('app'));
