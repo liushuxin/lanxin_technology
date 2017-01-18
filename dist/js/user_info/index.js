47e3b122a8168ef87db5af64b93e8d84c1b32bcd
liushuxin <shuxinliu2016@gmail.com>
2017-01-18 17:46:53 +0800

    simple dragula done

--- dist/js/user_info/index.js
+++ dist/js/user_info/index.js
@@ -1,7 +1,7 @@
 import React,{Component,propTypes} from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
-import 'dragula/dragula';
+import 'dragula/dist/dragula.css';
 import dragula from 'dragula';
 import IndexSass from '../../sass/index.scss';
 class App extends Component{
@@ -10,7 +10,7 @@ class App extends Component{
     this.state ={};
   }
   componentDidMount(){
-    dragula(document.getElementById('dr'), {
+    dragula([document.querySelector('#my1'),document.querySelector('#my2')], {
   isContainer: function (el) {
     return false; // only elements in drake.containers will be taken into account
   },
@@ -23,7 +23,7 @@ class App extends Component{
   invalid: function (el, handle) {
     return false; // don't prevent any drags from initiating by default
   },
-  direction: 'vertical',             // Y axis is considered when determining where an element would be dropped
+//  direction: 'vertical',             // Y axis is considered when determining where an element would be dropped
   copy: false,                       // elements are moved by default, not copied
   copySortSource: false,             // elements in copy-source containers can be reordered
   revertOnSpill: false,              // spilling will put the element back where it was dragged from, if this is true
@@ -35,9 +35,37 @@ class App extends Component{
   render(){
     let self = this;
     return (<div id="dr">
+      <div id="my1">
+ <div id="d1">
+      拖拽1
+    </div>
+    <div id="d2">
+      拖拽2
+    </div>
+     <div>
+      拖拽3
+    </div>
+     <div>
+      拖拽4
+    </div>
+     <div>
+      拖拽5
+    </div>
+    <span>
+       hahahaa
+     </span>
+     <div>
+      拖拽6
+    </div>
+
+      </div>
+      <div id="my2">
+     <span>
+       hahah
+     </span> 
+      </div>
+   
     
-    拖拽
-    div
     </div>);
   }
 
