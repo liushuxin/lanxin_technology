18654ff3f526313f004aaeee3ffe3ae367b7b22b
liushuxin <shuxinliu2016@gmail.com>
2017-04-06 10:44:32 +0800

    ignore hot-update

--- dist/js/user_info/index.js
+++ dist/js/user_info/index.js
@@ -11,46 +11,46 @@ class App extends Component{
   }
   componentDidMount(){
     dragula([document.querySelector('#my1'),document.querySelector('#my2')], {
-  isContainer: function (el) {
-    return el.classList.contains('dragula-container');
-  },
-  moves: function (el, source, handle, sibling) {
-    console.log(el);
-    console.log(source);
-    console.log(handle);
-    console.log(sibling);
-    return true; // elements are always draggable by default
-  },
-  accepts: function (el, target, source, sibling) {
-    console.log(el);
-    console.log(source);
-    console.log(target);
-    console.log(sibling);
-    return true; // elements can be dropped in any of the `containers` by default
-  },
-  invalid: function (el, handle) {
-    return false; // don't prevent any drags from initiating by default
-  },
-//  direction: 'vertical',             // Y axis is considered when determining where an element would be dropped
-  copy: true,                       // elements are moved by default, not copied
-  copySortSource: false,             // elements in copy-source containers can be reordered
-  revertOnSpill: false,              // spilling will put the element back where it was dragged from, if this is true
-  removeOnSpill: false,              // spilling will `.remove` the element, if this is true
-  mirrorContainer: document.body,    // set the element that gets mirror elements appended
-  ignoreInputTextSelection: true     // allows users to select input text, see details below
-});
+      isContainer: function (el) {
+        return el.classList.contains('dragula-container');
+      },
+      moves: function (el, source, handle, sibling) {
+        console.log(el);
+        console.log(source);
+        console.log(handle);
+        console.log(sibling);
+        return true; // elements are always draggable by default
+      },
+      accepts: function (el, target, source, sibling) {
+        console.log(el);
+        console.log(source);
+        console.log(target);
+        console.log(sibling);
+        return true; // elements can be dropped in any of the `containers` by default
+      },
+      invalid: function (el, handle) {
+        return false; // don't prevent any drags from initiating by default
+      },
+    //  direction: 'vertical',             // Y axis is considered when determining where an element would be dropped
+      copy: true,                       // elements are moved by default, not copied
+      copySortSource: false,             // elements in copy-source containers can be reordered
+      revertOnSpill: false,              // spilling will put the element back where it was dragged from, if this is true
+      removeOnSpill: false,              // spilling will `.remove` the element, if this is true
+      mirrorContainer: document.body,    // set the element that gets mirror elements appended
+      ignoreInputTextSelection: true     // allows users to select input text, see details below
+    });
   }
   render(){
     let self = this;
     return (<div id="dr">
-      <div id="my1">
- <div id="d1">
- <p>
-   拖拽1.1
- </p>
- <p>
-   拖拽1.2
- </p>
+    <div id="my1">
+       <div id="d1">
+       <p>
+         拖拽1.1
+       </p>
+       <p>
+         拖拽1.2
+       </p>
       
     </div>
     <div id="d2">
