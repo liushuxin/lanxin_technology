727e6569955bb89342e65eb78c6701e3a4a0d5e7
liushuxin <shuxinliu2016@gmail.com>
2017-05-08 23:42:02 +0800

    add bootstrap cdn

--- dist/js/user_info/content.js
+++ dist/js/user_info/content.js
@@ -3,15 +3,25 @@ import _ from 'lodash';
 export default class Content extends Component{
 constructor(props){
   super(props);
-  this.state = {}
+  this.state = {
+  	value:0
+  }
 }
 componentDidMount(){
+
 
 }
 render(){
   let self = this;
   return <div>
-    hah
+  <button className="btn btn-success" onClick={()=>{self.setState({value:self.state.value+1})}}>增加</button>
+   <Contents value={self.state.value}></Contents>
   </div>
 }
+}
+function Contents(props){
+	return <p>Contents组件的props.value:{props.value}</p>
+}
+Contents.propTypes ={
+	value:PropTypes.number.isRequired
 }
